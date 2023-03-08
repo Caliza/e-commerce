7c8e545a8f7b7445e0d4470a28d3872bce5753fe
Caliza <rances.rodriguez@gmail.com>
2023-03-08 13:11:09 -0800

    Added endpoints to category-routes.js.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,10 +6,22 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll().then((categoryData) => {
+    res.json(categoryData);
+  });
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
+  Category.findOne(
+    {
+      where: { 
+        id: req.params.id
+      },
+    }
+  ).then((categoryData) => {
+    res.json(categoryData);
+  });
   // be sure to include its associated Products
 });
 
@@ -19,10 +31,36 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  // Calls the update method on the Book model
+  Category.update(
+    {
+      // All the fields you can update and the data attached to the request body.
+      id: req.body.id,
+      category_name: req.body.category_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  ).then((updatedCategory) => {
+    // Sends the updated Category as a json response
+    res.json(updatedCategory);
+  })
+  .catch((err) => res.json(err));
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((deletedCategory) => {
+      res.json(deletedCategory);
+    })
+    .catch((err) => res.json(err))
 });
 
 module.exports = router;
