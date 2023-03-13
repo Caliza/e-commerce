21def7e432b248e0fe2068cf5bc5bb7a5eb472f4
Caliza <rances.rodriguez@gmail.com>
2023-03-13 13:03:09 -0700

    refactored category.js.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -18,6 +18,9 @@ router.get('/:id', (req, res) => {
       where: {
         id: req.params.id
       },
+      include: [
+        Product
+      ],
     }
   ).then((categoryData) => {
     res.json(categoryData);
